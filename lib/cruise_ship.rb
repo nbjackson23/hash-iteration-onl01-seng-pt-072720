c8b9893902fb09bcb86aacfebc5ed850aac3b9f3
Nancy Jackson <nbjackson23@gmail.com>
2020-08-08 15:56:14 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,11 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
+ winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
-  winner
 end
  
+winner
